2a2f4f7d0e303465c9ae27af0ad1e1f56a0b0377
Liam <liam.c.bell@outlook.com>
2020-04-06 18:53:11 -0400

    Init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { TodoPageComponent } from './components/todo-page/todo-page.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { RegistrationComponent } from './components/registration/registration.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: "/loginPage",
+  pathMatch: 'full'
+},
+{
+  path: "loginPage", component: LoginPageComponent
+},
+{
+  path: "todoList", component: TodoPageComponent
+},
+{ path: "registration", component: RegistrationComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { FormsModule } from '@angular/forms';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { TodoPageComponent } from './components/todo-page/todo-page.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    LoginPageComponent,
+    RegistrationComponent,
+    TodoPageComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
